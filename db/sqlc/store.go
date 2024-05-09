c71a12eab459b2ff1cec1524acc06a5f0871963d
Leon David Zipp <leondavidzipp@gmx.de>
2024-05-09 23:16:39 +0200

    added more functionality to execTransaction

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -4,22 +4,81 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
-	// mongodb "go.mongodb.org/mongo-driver/mongo"
+	mongodb "go.mongodb.org/mongo-driver/mongo"
 )
 
 
 type Store struct {
 	Querier
 	UserDB *sql.DB
-	// ImageDBClient *mongodb.Client
+	ImageDBClient *mongodb.Client
 }
 
-func (store *Store) execTransaction(ctx context.Context fn func(*Queries) error) error {
-	sql_tx, err := store.UserDB.BeginTx(ctx, nil)
+func callback(sessionContext mongo.SessionContext, fn func(mongo.SessionContext) error) (interface{}, error) {
+	err := fn(sessionContext)
+	if err != nil {
+		return nil, err
+	}
+	return nil, nil
+}
+
+func (store *Store) execTransaction(ctx context.Context,
+	filepath string,
+	fnSql func(*Queries) error,
+	fnMongo func(mongo.SessionContext, string) error) error {
+	// postgres
+	sqlTransaction, err := store.UserDB.BeginTx(ctx, nil)
 	if err != nil {
 		return err
 	}
 
-	sql_querie := New(sql_tx)
-	err = fn
+	sqlQuerie := New(sqlTransaction)
+	err = fnSql(sqlQuerie)
+	if err != nil {
+		if rollbackErr := sqlTransaction.Rollback(), rollbackErr != nil {
+			return fmt.Errorf("transaction err: %v; rollback err: %v", err, rollbackErr)
+		}
+		return err
+	}
+
+	// mongodb
+	session, err := store.ImageDBClient.StartSession()
+	if err != nil {
+		if rollbackErr := sqlTransaction.Rollback(), rollbackErr != nil {
+			return fmt.Errorf("transaction err: %v; rollback err: %v", err, rollbackErr)
+		}
+		return err
+	}
+	defer session.EndSession()
+
+	err = session.StartTransaction()
+	if err != nil {
+		if rollbackErr := sqlTransaction.Rollback(), rollbackErr != nil {
+			return fmt.Errorf("transaction err: %v; rollback err: %v", err, rollbackErr)
+		}
+		return err
+	}
+
+	// TODO: move database into fnMongo, implement uploading image in fnMongo
+	err = mongo.WithSession(ctx, session, func(sessionContext mongo.SessionContext) error {
+		return fnMongo(sessionContext, filePath) // Pass file path to fnMongo
+	})
+	if er != nil {
+		if rollbackErr := sqlTransaction.Rollback(), rollbackErr != nil {
+			return fmt.Errorf("transaction err: %v; rollback err: %v", err, rollbackErr)
+		}
+		return err
+	}
+	mongoDB, err := ImageDBClient.Database()
+
+	// commit
+	err = sqlTransaction.Commit()
+	if err != nil {
+		return err
+	}
+	err = session.CommitTransaction(ctx)
+	if err != nil {
+		return err
+	}
+	return nil 
 }
