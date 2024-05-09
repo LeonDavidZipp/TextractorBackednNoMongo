e2e4e0aa2a30b6a3f95b0198e3ed5eccd42510ec
LeondavidZipp <leondavidzipp@gmx.de>
2024-05-09 12:56:36 +0200

    added first tests for database, now switching to another branch

---
+++ db/sqlc/account_test.sql.go
@@ -0,0 +1,20 @@
+package db
+
+import (
+	"context"
+	"testing"
+	"time"
+	"database/sql"
+	"github.com/stretchr/testify/require"
+	"github.com/LeonDavidZipp/Textractor"
+)
+
+
+func createRandomAccount(t *testing.T) Account {
+	arg := CreateAccountParams{
+		Owner : ,
+		Email : ,
+		GoogleID : nil,
+		FacebookID : nil,
+	}
+}
