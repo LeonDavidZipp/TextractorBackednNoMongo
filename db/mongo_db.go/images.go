61ed739be4b3bf80a03cdeab860567d1fa0f807e
LeondavidZipp <leondavidzipp@gmx.de>
2024-05-10 18:02:09 +0200

    added FindImage, updated InsertImage

--- db/mongo_db.go/images.go
+++ db/mongo_db.go/images.go
@@ -4,11 +4,11 @@ import (
 	"database/sql"
 	"fmt"
 	mongodb "go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 
 type InsertImageParams struct {
-	ID        primitive.ObjectID `bson:"_id" json:"id"`
 	AccountID Account            `bson:"account_id" json:"account_id"`
 	Text      string             `bson:"text" json:"text"`
 	// link to the image in s3 storage
@@ -18,13 +18,8 @@ type InsertImageParams struct {
 	Image64   string             `bson:"image_64" json:"image_64"`
 }
 
-type InsertImageResult struct {
-	ID        primitive.ObjectID `bson:"_id" json:"id"`
-	AccountID Account            `bson:"account_id" json:"account_id"`
-	Text      string             `bson:"text" json:"text"`
-	Image64   string             `bson:"image_64" json:"image_64"`
-}
-
+// InsertImage inserts a new image into the database
+// TODO: implement not for Store but for Mongo "Queries"
 func (store *Store) InsertImage(
 	ctx context.Context,
 	arg InsertImageParams
@@ -35,16 +30,25 @@ func (store *Store) InsertImage(
 		return UploadImageTransactionResult{}, fmt.Errorf("Could not insert image: %w", err)
 	}
 
-	result := InsertImageResult{
-		ID:        imageID.InsertedID.(primitive.ObjectID),
+	result := Image{
+		ID:        imageID,
 		AccountID: arg.AccountID,
 		Text:      arg.Text,
+		Link:      arg.Link,
+		CreatedAt: arg.CreatedAt,
 		Image64:   arg.Image64,
 	}
 
 	return result, nil
 }
 
-func (store *Store) GetImage(ctx context.Context, id string) (*Image, error) {
-
+func (store *Store) FindImage(ctx context.Context, id primitive.ObjectID) (Image, error) {
+	collection := store.ImageDB.Collection("images")
+	filter := bson.M{"_id": id}
+	var image Image
+	err := collection.FindOne(ctx, filter).Decode(&image)
+	if err != nil {
+		return Image{}, fmt.Errorf("Could not find image: %w", err)
+	}
+	return image, nil
 }
