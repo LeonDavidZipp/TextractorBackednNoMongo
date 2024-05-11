0af56bb09f143a1ecc7199eed8b014f1f32eb177
LeondavidZipp <leondavidzipp@gmx.de>
2024-05-11 11:11:53 +0200

    added update image methods

--- db/mongo_db.go/images.go
+++ db/mongo_db.go/images.go
@@ -9,12 +9,11 @@ import (
 
 
 type InsertImageParams struct {
-	AccountID Account            `bson:"account_id" json:"account_id"`
+	AccountID int64              `bson:"account_id" json:"account_id"`
 	Text      string             `bson:"text" json:"text"`
 	// link to the image in s3 storage
 	Link      string             `bson:"link" json:"link"`
 	CreatedAt time.Time          `bson:"created_at" json:"created_at"`
-	// base64 encoded image
 	Image64   string             `bson:"image_64" json:"image_64"`
 }
 
@@ -23,15 +22,16 @@ type InsertImageParams struct {
 func (store *Store) InsertImage(
 	ctx context.Context,
 	arg InsertImageParams
-) (InsertImageResult, error) {
+) (Image, error) {
 	collection := store.ImageDB.Collection("images")
-	imageID, err := collection.InsertOne(ctx, arg)
+	inserted, err := collection.insertOne(ctx, arg)
+
 	if err != nil {
-		return UploadImageTransactionResult{}, fmt.Errorf("Could not insert image: %w", err)
+		return Image{}, fmt.Errorf("Could not insert image: %w", err)
 	}
 
-	result := Image{
-		ID:        imageID,
+	image := Image{
+		ID:        inserted.imageID,
 		AccountID: arg.AccountID,
 		Text:      arg.Text,
 		Link:      arg.Link,
@@ -39,16 +39,47 @@ func (store *Store) InsertImage(
 		Image64:   arg.Image64,
 	}
 
-	return result, nil
+	return image, nil
 }
 
 func (store *Store) FindImage(ctx context.Context, id primitive.ObjectID) (Image, error) {
 	collection := store.ImageDB.Collection("images")
 	filter := bson.M{"_id": id}
+
 	var image Image
-	err := collection.FindOne(ctx, filter).Decode(&image)
+	image, err := collection.FindOne(ctx, filter).Decode(&image)
 	if err != nil {
 		return Image{}, fmt.Errorf("Could not find image: %w", err)
 	}
+	return image, nil
+}
+
+func (store *Store) DeleteImage(ctx context.Context, id primitive.ObjectID) error {
+	collection := store.ImageDB.Collection("images")
+	filter := bson.M{"_id": id}
+
+	_, err := collection.DeleteOne(ctx, filter)
+	if err != nil {
+		return fmt.Errorf("Could not delete image: %w", err)
+	}
+
+	return nil
+}
+
+type UpdateImageParams struct {
+	ImageID   primitive.ObjectID `bson:"image_id" json:"image_id"`
+	Text      string             `bson:"text" json:"text"`
+	Image64   string             `bson:"image_64" json:"image_64"`
+}
+
+func (store *Store) UpdateImage(ctx context.Context, arg UpdateImageParams) (Image, error) {
+	collection := store.ImageDB.Collection("images")
+	filter := bson.M("_id": arg.ImageID)
+
+	image, err := collection.updateOne(ctx, filter, arg)
+	if err != nil {
+		return fmt.Errorf("Could not update image: %w", err)
+	}
+
 	return image, nil
 }
--- db/mongo_db.go/models.go
+++ db/mongo_db.go/models.go
@@ -12,11 +12,10 @@ import (
 // only for learning, not most efficient way to store
 type Image struct {
 	ID        primitive.ObjectID `bson:"_id" json:"id"`
-	AccountID Account            `bson:"account_id" json:"account_id"`
+	AccountID int64              `bson:"account_id" json:"account_id"`
 	Text      string             `bson:"text" json:"text"`
 	// link to the image in s3 storage
 	Link      string             `bson:"link" json:"link"`
 	CreatedAt time.Time          `bson:"created_at" json:"created_at"`
-	// base64 encoded image
 	Image64   string             `bson:"image_64" json:"image_64"`
 }
