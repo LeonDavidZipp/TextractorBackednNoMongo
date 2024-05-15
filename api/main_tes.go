4d823e797ca50a882b1f81d40b78d7e955217465
LeondavidZipp <leondavidzipp@gmx.de>
2024-05-15 17:35:51 +0200

    connection working again

--- api/main_tes.go
+++ api/main_tes.go
@@ -4,47 +4,45 @@ import (
 	_ "github.com/lib/pq"
 	"context"
 	"os"
+	"time"
 	"log"
 	"testing"
+	"database/sql"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	db "github.com/LeonDavidZipp/Textractor/db"
-	"database/sql"
-	sqldb "github.com/LeonDavidZipp/Textractor/db/sql_db"
-	mongodb "github.com/LeonDavidZipp/Textractor/db/mongo_db"
+	st "github.com/LeonDavidZipp/Textractor/db/store"
 )
 
 
 func TestMain(m *testing.M) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10 * time.Second)
 	defer cancel()
-	config, err := util.LoadConfig(".")
-	if err != nil {
-		log.Fatal("Cannot load config:", err)
-	}
 
-	userDB, err := sql.Open(config.DBDriver, config.DBSource)
+	userDB, err := sql.Open(
+		os.Getenv("POSTGRES_DRIVER"),
+		os.Getenv("POSTGRES_SOURCE"),
+	)
 	if err != nil {
 		log.Fatal("Cannot connect to User DB:", err)
 	}
 
-	optionsClient := options.Client().ApplyURI(config.MongoSource)
+	optionsClient := options.Client().ApplyURI(os.Getenv("MONGO_SOURCE"))
 	mongoClient, err := mongo.Connect(ctx, optionsClient)
 	if err != nil {
 		log.Fatal("Cannot connect to Image DB:", err)
 	}
 	defer mongoClient.Disconnect(ctx)
 
-	imageDB := mongoClient.Database(config.MongoDBName)
+	imageDB := mongoClient.Database(os.Getenv("MONGO_DB_NAME"))
 	err = imageDB.Client().Ping(ctx, nil)
 	if err != nil {
 		log.Fatal("Image DB not reachable:", err)
 	}
 
-	store := db.NewStore(userDB, imageDB)
-	server := api.NewServer(store)
+	store := st.NewStore(userDB, imageDB)
+	server := NewServer(store)
 
-	err = server.Start(config.ServerAddress)
+	err = server.Start(os.Getenv("SERVER_ADDRESS"))
 	if err != nil {
 		log.Fatal("Cannot start server:", err)
 	}
