7adfd32cc6c61a138a711fe1070fd0b36c1ccbbd
LeondavidZipp <leondavidzipp@gmx.de>
2024-05-16 16:21:43 +0200

    updated s3_bucket.go

--- db/s3_bucket/s3_bucket.go
+++ db/s3_bucket/s3_bucket.go
@@ -1,11 +1,3 @@
 package db
 
-import (
-	"context"
-	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/service/s3"
-	"github.com/aws/aws-sdk-go-v2/feature/s3/manager"
-	"github.com/aws/aws-sdk-go-v2/config"
-)
-
-
+// Not needed for this task, different to postgres & mongodb
