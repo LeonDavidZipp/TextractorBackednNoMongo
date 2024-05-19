d64a8b5a1c027d8f65c816de24bd4166a8591474
Leon David Zipp <leondavidzipp@gmx.de>
2024-05-19 14:26:05 +0200

    updated sqlc

--- db/migrations/000001_init_schema.up.sql
+++ db/migrations/000001_init_schema.up.sql
@@ -1,27 +1,16 @@
--- Use to initialize empty DB and tables OR reset DB and tables
-
--- DO
--- $$
--- BEGIN
---     IF EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'userdb_user') THEN
---         DROP ROLE userdb_user;
---     END IF;
--- END
--- $$;
--- CREATE ROLE userdb_user WITH LOGIN PASSWORD 'test1234';
--- DROP DATABASE IF EXISTS userdb;
--- CREATE DATABASE userdb;
--- GRANT ALL PRIVILEGES ON DATABASE userdb TO userdb_user;
-
 CREATE TABLE IF NOT EXISTS "accounts" (
   "id" BIGSERIAL PRIMARY KEY,
   "owner" varchar NOT NULL,
-  "email" varchar NOT NULL,
+  "email" varchar NOT NULL UNIQUE,
   "google_id" varchar DEFAULT NULL,
   "facebook_id" varchar DEFAULT NULL,
   "image_count" bigint NOT NULL DEFAULT 0,
   "subscribed" bool NOT NULL DEFAULT false,
   "created_at" timestamptz NOT NULL DEFAULT (now())
+  CHECK (
+    (google_id IS NOT NULL AND facebook_id IS NULL) OR 
+    (google_id IS NULL AND facebook_id IS NOT NULL)
+  )
 );
 
 CREATE INDEX ON "accounts" ("owner");
